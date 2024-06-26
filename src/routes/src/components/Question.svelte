c7a872568e657287cd8b1356ddb35169390e018a
banany228 <maxlandov@gmail.com>
2024-06-26 21:33:30 +0200

    Create Question.svelte

---
+++ src/routes/src/components/Question.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let question;
+  export let selected;
+</script>
+
+<div class="question">
+  <p>{question.question}</p>
+  <ul>
+    {#each question.options as option}
+      <li>
+        <label>
+          <input type="radio" bind:checked={selected} value={option} />
+          {option}
+        </label>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  /* Add your CSS styles here */
+</style>
